fb4c678e6e278ef19d7f756fca7ca08b6801cd90
Noura Alotaibi <57773995+Nourabe8@users.noreply.github.com>
2025-01-15 16:48:20 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,35 @@ variable "pm_tls_insecure" {
   default = true
 }
 
+variable "network_model" {
+  description = "The network model for the VM"
+  type        = string
+  default     = "virtio"  # Default network model
+}
+
+variable "network_bridge" {
+  description = "The network bridge for the VM"
+  type        = string
+  default     = "vmbr0"  # Default network bridge
+}
+
+variable "proxmox_ip" {
+  description = "The static IP address for the VM"
+  type        = string
+}
+
+variable "gateway" {
+  description = "The gateway for the network"
+  type        = string
+}
+
+variable "target_node" {
+  description = "The target Proxmox node"
+  type        = string
+}
+
+variable "clone_template" {
+  description = "The Proxmox template to clone from"
+  type        = string
+}
+
